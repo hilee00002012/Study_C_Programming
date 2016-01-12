5a2d6bd7323373f7e17f66e9d3d375deba69f572
hi <yoonhwan39@naver.com>
2016-01-12 13:59:29 +0900

    프로그램 사용자로부터 계속해서 정수를 입력 받는다. 그리고 그 값을 계속해서 더해 나간다. 이러한 작업은 사용자가 0을 입력할 때까지 계속되어야 하며, 0이 입력되면 입력된 모든 정수의 합을 출력하고 프로그램을 종료시킨다.

---
+++ while_Q3.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int main() {
+	int input, sum = 0;
+
+	while(input != 0) {
+		printf("입력 : ");
+		scanf("%d", &input);
+
+		sum = sum + input;
+		printf("sum = %d\n", sum );
+	}
+
+	return 0;
+}
